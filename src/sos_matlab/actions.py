f457b14bef6df405be1d2526ed4806ef9faf1323
Bo Peng <ben.bog@gmail.com>
2017-11-15 07:01:25 -0600

    Update matlab action command line option

--- src/sos_matlab/actions.py
+++ src/sos_matlab/actions.py
@@ -23,9 +23,9 @@
 from sos.actions import SoS_Action, SoS_ExecuteScript
 
 @SoS_Action(acceptable_args=['script', 'args'])
-def matlab(script, args='-nojvm -nodisplay -nosplash -r ${filename!n};quit', **kwargs):
+def matlab(script, args='''-nojvm -nodisplay -nosplash -nodesktop -r "try, run('{filename}'), catch me, fprintf('%s / %s\\n',me.identifier,me.message), exit(1), end, exit(0);"''', **kwargs):
     '''Execute specified script with command Matlab, with default options
-    "-nodisplay -r". This action accepts common action arguments such as input,
+    "-nojvm -nodisplay -nosplash -nodesktop -r". This action accepts common action arguments such as input,
     active, workdir, docker_image and args. In particular, content of one or more 
     files  specified by option input would be
     prepended before the specified script.
