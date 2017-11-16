f5572468eab77f7afcaa70bc7552f3a47f4b3068
Bo Peng <ben.bog@gmail.com>
2017-11-15 20:12:13 -0600

    Fix tests for the rename of MATLAB kernel

--- test/test_Matlab_kernel.py
+++ test/test_Matlab_kernel.py
@@ -55,7 +55,7 @@ arr[::10] = np.nan
 df = pd.DataFrame({'column_{0}'.format(i): arr for i in range(10)})
 ''')
             clear_channels(iopub)
-            execute(kc=kc, code="%use Matlab")
+            execute(kc=kc, code="%use MATLAB")
             wait_for_idle(kc)
             #_, stderr = assemble_output(iopub)
             #self.assertEqual(stderr, '')
@@ -90,7 +90,7 @@ recursive_var = {'a': {'b': 123}, 'c': True}
 ''')
             wait_for_idle(kc)
             execute(kc=kc, code='''
-%use Matlab
+%use MATLAB
 %get null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var set_var list_var dict_var recursive_var float_var
 %dict -r
 %put null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var set_var list_var dict_var recursive_var float_var
@@ -104,7 +104,7 @@ recursive_var = {'a': {'b': 123}, 'c': True}
             self.assertEqual(res['logic_var'], True)
             self.assertEqual(res['logic_arr_var'], [True, False, True])
             self.assertEqual(res['char_var'], '1"23')
-            self.assertEqual(res['float_var'], [1.2, 2.1])
+            self.assertEqual(list(res['float_var']), [1.2, 2.1])
             self.assertEqual(list(res['list_var']), [1,2,'3'], 'Got {}'.format(res['list_var']))
             self.assertEqual(res['dict_var'], {'a': 1, 'b': 2, 'c': '3'})
             self.assertTrue(len(res['set_var']) ==3 and '3' in res['set_var'] and 1 in res['set_var'] and 2 in res['set_var'])
@@ -118,7 +118,7 @@ recursive_var = {'a': {'b': 123}, 'c': True}
         with sos_kernel() as kc:
             iopub = kc.iopub_channel
             execute(kc=kc, code="""
-%use Matlab
+%use MATLAB
 null_var = NaN
 num_var = 123
 num_arr_var = [1, 2, 3]
