4c0af397aa9098ea27a6314d4d0cba79aaec1cf0
Bo Peng <ben.bog@gmail.com>
2019-05-03 01:12:30 -0500

    Release sos matlab 0.19.3

--- src/sos_matlab/_version.py
+++ src/sos_matlab/_version.py
@@ -3,23 +3,5 @@
 # Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
 # Distributed under the terms of the 3-clause BSD License.
 
-import sys
 
-__all__ = ['__version__', 'SOS_FULL_VERSION']
-
-_py_ver = sys.version_info
-if _py_ver.major == 2 or (_py_ver.major == 3 and (_py_ver.minor, _py_ver.micro) < (4, 0)):
-    raise SystemError('SOS requires Python 3.4 or higher. Please upgrade your Python {}.{}.{}.'
-        .format(_py_ver.major, _py_ver.minor, _py_ver.micro))
-
-
-# version of the SoS language
-__sos_version__ = '1.0'
-# version of the sos command
-__version__ = '0.18.3'
-__py_version__ = '{}.{}.{}'.format(_py_ver.major, _py_ver.minor, _py_ver.micro)
-
-#
-SOS_FULL_VERSION='{} for Python {}.{}.{}'.format(__version__, _py_ver.major, _py_ver.minor, _py_ver.micro)
-SOS_COPYRIGHT = '''SoS {} : Copyright (c) 2016 Bo Peng'''.format(__version__)
-SOS_CONTACT = '''Please visit http://github.com/vatlab/SOS for more information.'''
+__version__ = '0.18.4'
